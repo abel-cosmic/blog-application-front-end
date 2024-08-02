237d02c62a81a54208c557b96495e3a4c8d74d06
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-08-02 21:12:40 +0300

    fix: changed img to Image

--- src/app/(pages)/blog/[id]/page.tsx
+++ src/app/(pages)/blog/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useParams } from "next/navigation";
 import { Badge } from "@/components/ui/badge";
 import { blogItems } from "@/configs/objects/blog";
+import Image from "next/image"
 
 export default function BlogDetail() {
   const { id } = useParams();
@@ -15,9 +16,11 @@ export default function BlogDetail() {
 
   return (
     <div className="md:px-20 px-10 gap-4 md:gap-10 flex flex-col md:flex-row md:items-center max-md:pt-20">
-      <img
+      <Image
         src={blog.image.src}
         alt={blog.title}
+        width={0}
+        height={0}
         className="md:w-1/2 max-md:w-full md:h-[35rem] max-md:h-96 object-cover rounded-lg mb-4"
       />
       <div className="flex flex-col gap-4 md:w-2/3 max-md:w-full">
